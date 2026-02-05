2aa125a73b78bb65878f015934ef1f9a95295cea
Tên của bạn <tranlychi86@gmail.com>
2026-02-05 14:30:34 +0700

    Fix module resolution for production build

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -12,6 +12,12 @@ const nextConfig = {
             },
         ];
     },
+    webpack: (config) => {
+        config.resolve.extensionAlias = {
+            ".js": [".ts", ".tsx", ".js", ".jsx"],
+        };
+        return config;
+    },
 };
 
 module.exports = nextConfig;
